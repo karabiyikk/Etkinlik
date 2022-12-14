0fbb22319945c34120e9a2cac0204942d62cb4aa
Alper Karabıyık <120407040+karabiyikk@users.noreply.github.com>
2022-12-14 12:42:11 +0300

    Add files via upload

---
+++ Onerme.java
@@ -0,0 +1,31 @@
+package KosulluIfadeler;
+
+import java.util.Scanner;
+//Sıcaklık 5'dan küçük ise "Kayak" yapmayı öner.
+//Sıcaklık 5 ve 15 arasında ise "Sinema" etkinliğini öner.
+//Sıcaklık 10 ve 25 arasında ise "Piknik" etkinliğini öner.
+//Sıcaklık 25'ten büyük ise "Yüzme" etkinliğini öner.
+//Aynı örnek üzerinden if koşulları başka hangi şekilde oluşturulabilirdi farklı çözüm yolları bulunuz.
+
+public class Onerme {
+    public static void main(String[] args) {
+        int heat;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Sıcaklık Giriniz : ");
+        heat = input.nextInt();
+
+        if (heat < 5) {
+            System.out.println("Kayak yapabilirsiniz");
+        } else if (heat <= 25) {
+            if (heat < 15) {
+                System.out.println("Sinemaya gidebilirsiniz");
+            }
+            if (heat >= 10) {
+                System.out.println("Pikniğe gidebilirsiniz");
+            }
+        } else {
+            System.out.println("Yüsmeye gidebilirsiniz");
+        }
+    }
+}
